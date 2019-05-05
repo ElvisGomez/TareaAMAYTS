1a2c77c3dd4c500081434a1bb4a6601f6061655b
Hernandez <Hernandez@192.168.0.18>
2019-05-05 07:57:31 -0600

    que tal

--- Ejercicio5/ejercicio5.php
+++ Ejercicio5/ejercicio5.php
@@ -1,4 +1,20 @@
 <?php
 
 
+ echo "<h4>Conversion de acres a hectares</h4>";
+
+ $acres = 1000;
+ $hectarea = 1.0;
+
+ echo "El numero de acres son: " . $acres;
+ echo "<br><br>";
+
+ $conversion = 0;
+ $conversion = round($acres*($hectarea/2.4711),2);
+
+ echo $acres . " acres son " . $conversion . " hectareas."; 
+
+?>
+
+
 
